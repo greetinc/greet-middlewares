be60a26872a6e3d298c5303f107c9fd456f1ec0f
Asep rayana <asep.rayana09@gmail.com>
2024-04-06 07:20:39 +0700

    first commit

--- middlewares/auth.middleware.go
+++ middlewares/auth.middleware.go
@@ -7,33 +7,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// func AuthorizeJWT(jwtService JWTService) echo.MiddlewareFunc {
-// 	return func(next echo.HandlerFunc) echo.HandlerFunc {
-// 		return func(c echo.Context) error {
-// 			authHeader := c.Request().Header.Get("Authorization")
-// 			if authHeader == "" {
-// 				return res.ErrorBuilder(&res.ErrorConstant.Unauthorized, nil).Send(c)
-// 			}
-
-// 			// ValidateToken returns the token, userID, and an error
-// 			token, _, err := jwtService.ValidateToken(authHeader)
-// 			if err != nil {
-// 				// Handle the error, e.g., log it or return an internal server error
-// 				return err
-// 			}
-
-// 			// Now you have the token and userID for further processing
-// 			if token.Valid {
-// 				claims := token.Claims.(jwt.MapClaims)
-// 				log.Println("Claim[name]: ", claims["name"])
-// 				return next(c)
-// 			} else {
-// 				return res.ErrorBuilder(&res.ErrorConstant.BadRequest, &res.Error{}).Send(c)
-// 			}
-// 		}
-// 	}
-// }
-
 func AuthorizeJWT(jwtService JWTService) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
